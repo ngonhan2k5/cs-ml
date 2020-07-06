43df669e93b0b223da8a99d6a12446d951f4046c
datvt9312 <datvt9312@gmail.com>
2020-07-06 16:03:27 -0500

    init pro2

---
+++ pro2/frontend/pages/index.js
@@ -0,0 +1,93 @@
+import { useState, useEffect } from "react";
+import Head from "next/head";
+import Movie from "../components/Movie";
+
+const API_KEY = "ce762116";
+export default function Home() {
+  const [q, setQuery] = useState("batman");
+
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(null);
+    setData(null);
+
+    fetch(`http://www.omdbapi.com/?s=${q}&apikey=${API_KEY}`)
+      .then((resp) => resp)
+      .then((resp) => resp.json())
+      .then((response) => {
+        if (response.Response === "False") {
+          setError(response.Error);
+        } else {
+          console.log("response.Search");
+          console.log(response.Search);
+          setData(response.Search);
+        }
+
+        setLoading(false);
+      })
+      .catch(({ message }) => {
+        setError(message);
+        setLoading(false);
+      });
+  }, [q]);
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+          crossorigin="anonymous"
+        ></link>
+      </Head>
+
+      <main>
+        <h1>MOVIES RECOMMENDATION</h1>
+        {loading ?? <span>Loading...</span>}
+        <div className="container row">
+          {data?.map((movie) => (
+            <Movie movie={movie} key={movie.imdbID}></Movie>
+          ))}
+        </div>
+      </main>
+
+      <footer></footer>
+
+      <style jsx>{`
+        main > h1 {
+          text-align: center;
+          padding: 10px;
+        }
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
+}
