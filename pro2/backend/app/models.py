ac955cf5bab4591147a4d9d9dc0c80c7a34a3e4f
datvt9312 <datvt9312@gmail.com>
2020-07-07 09:41:08 -0500

    update modal

--- pro2/backend/app/models.py
+++ pro2/backend/app/models.py
@@ -4,14 +4,26 @@ from app import db, ma
 class Movie(db.Model):
     __tablename__ = 'movies'
     id = db.Column(db.Integer, primary_key=True)
-    poster_path = db.Column(db.String(80), unique=True)
-    title = db.Column(db.String(120), unique=True)
+    imdb_id = db.Column(db.Integer, primary_key=True)
+    poster_path = db.Column(db.String(80))
+    title = db.Column(db.String(120))
+    overview = db.Column(db.String(120))
+    vote_average = db.Column(db.Float)
+    vote_count = db.Column(db.Integer)
+    runtime = db.Column(db.Integer)
 
-    def __init__(self, poster_path, title):
+    def __init__(self, poster_path, imdb_id, title, overview, vote_average,
+                 vote_count, runtime):
         self.poster_path = poster_path
+        self.imdb_id = imdb_id
         self.title = title
+        self.overview = overview
+        self.vote_average = vote_average
+        self.vote_count = vote_count
+        self.runtime = runtime
 
 
 class MovieSchema(ma.Schema):
     class Meta:
-        fields = ('poster_path', 'title')
+        fields = ('poster_path', 'imdb_id', 'title', 'overview',
+                  'vote_average', 'vote_count', 'runtime')
