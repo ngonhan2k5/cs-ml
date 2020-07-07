7091acf21b144760f59713ae9afefac2acdd83e5
datvt9312 <datvt9312@gmail.com>
2020-07-07 12:18:47 -0500

    update git ignore & front end

--- pro2/backend/app/models.py
+++ pro2/backend/app/models.py
@@ -11,9 +11,10 @@ class Movie(db.Model):
     vote_average = db.Column(db.Float)
     vote_count = db.Column(db.Integer)
     runtime = db.Column(db.Integer)
+    release_date = db.Column(db.String(15))
 
     def __init__(self, poster_path, imdb_id, title, overview, vote_average,
-                 vote_count, runtime):
+                 vote_count, runtime, release_date):
         self.poster_path = poster_path
         self.imdb_id = imdb_id
         self.title = title
@@ -21,9 +22,10 @@ class Movie(db.Model):
         self.vote_average = vote_average
         self.vote_count = vote_count
         self.runtime = runtime
+        self.release_date = release_date
 
 
 class MovieSchema(ma.Schema):
     class Meta:
         fields = ('poster_path', 'imdb_id', 'title', 'overview',
-                  'vote_average', 'vote_count', 'runtime')
+                  'vote_average', 'vote_count', 'runtime', 'release_date')
