ae4221f7880ef507fb3b7ed72eb29f9b9ecf8d76
Ngo Nhan <ngonhan2k5@gmail.com>
2020-07-07 10:16:34 -0500

    Release model2 with gzip

---
+++ pro2/release/model2-test.py
@@ -0,0 +1,18 @@
+import pickle
+import pandas as pd 
+
+df2=pd.read_csv('../input/tmdb-movie-metadata/tmdb_5000_movies.csv')
+
+from model2 import Model2
+
+# Test
+id = 155
+print(df2.loc[df2['id']==id][['title', 'overview']])
+
+# Import
+with open('../backend/ml_models/model2.pickle', 'rb') as f:
+    model2 = pickle.load(f)
+    
+    # Test model call
+    print(model2.get_recommendations(id).to_json())
+    # print(json.dumps(model2.get_recommendations(id)))
