059a32211e7a323b042a2f5ef761087ff757aa02
datvt9312 <datvt9312@gmail.com>
2020-07-07 09:41:08 -0500

    update modal

--- pro2/backend/app/utils.py
+++ pro2/backend/app/utils.py
@@ -1,6 +1,3 @@
-from flask import request
-
-
 def is_integer(s):
     try:
         int(s)
@@ -8,11 +5,13 @@ def is_integer(s):
     except ValueError:
         return False
 
+
 def check_request_argument_format(user_id, movie_id):
     if not is_integer(user_id) or \
        not is_integer(movie_id):
         return False
     return True
+
 
 def check_input_valid(user_id, movie_id, ratings):
     users = ratings.query('userId ==' + str(user_id))
@@ -20,5 +19,3 @@ def check_input_valid(user_id, movie_id, ratings):
     if len(users) == 0 or len(movies) == 0:
         return False
     return True
-
-
