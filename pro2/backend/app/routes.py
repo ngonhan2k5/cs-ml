633ebb472a7d153c203cbe5c920f8f8c503f4bd5
cuongnm92 <cuongnm92vn@gmail.com>
2020-07-07 06:03:23 -0700

    update api for finding top 10 movies of user

--- pro2/backend/app/routes.py
+++ pro2/backend/app/routes.py
@@ -1,5 +1,6 @@
 from app import app
 from app.models import MovieSchema, Movie
+from app.top_rate_for_user import TopRateMovieForUser
 from app.utils import check_input_valid
 from flask import abort, g, jsonify, request
 import pickle
@@ -64,11 +65,14 @@ def get_rate():
     return jsonify(predict)
 
 
-@app.route('/api/top-ten-rate/<movie_id>')
-def get_top_ten_rate(Movie_id):
-    # return top 10 movies' movie_ids
-    top_ten_rate = ['tt0114709', 'tt0113497']
-    return jsonify(top_ten_rate)
+@app.route('/api/top-ten-rate', methods=['GET'])
+def get_top_ten_rate_of_user():
+    # return top 10 movies' with user_id
+    user_id = int(request.args['user_id'])
+    print(user_id)
+    topRateMovieForUser = TopRateMovieForUser('ml_data/','ratings_small.csv', 'ml_models/', 'top-user-movie-ratings-small.pkl')
+    # top_ten_rate = ['tt0114709', 'tt0113497']
+    return jsonify(topRateMovieForUser.get_top_ten_rate_of_user(user_id))
 
 
 @app.route('/api/movies/<movie_id>')
