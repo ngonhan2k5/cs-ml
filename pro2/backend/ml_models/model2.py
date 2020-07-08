6f11e3ff57d902e5854be1be86000ba55e646a27
Ngo Nhan <ngonhan2k5@gmail.com>
2020-07-07 23:05:53 -0500

    Try to use same data set

--- pro2/backend/ml_models/model2.py
+++ pro2/backend/ml_models/model2.py
@@ -5,6 +5,7 @@ class Model2:
     def __init__(self, movie_csv_path):
 
         df2=pd.read_csv(movie_csv_path)
+        df2.drop(df2.columns.difference(['overview','id']), 1, inplace=True)
         self.index_movieid = pd.Series(df2.index, index=df2['id']).drop_duplicates()
         self.cosine_sim = self.create_cosine_sim_matrix(df2)
 
@@ -34,23 +35,25 @@ class Model2:
     # Function that takes in movie title as input and outputs most similar movies
     def get_recommendations(self, movie_id):
         cosine_sim=self.cosine_sim
+
         # Get the index of the movie that matches the title
-        idx = self.index_movieid[movie_id]
+        if movie_id in self.index_movieid:
+            idx = self.index_movieid[movie_id]
 
+            # Get the pairwsie similarity scores of all movies with that movie
+            sim_scores = list(enumerate(cosine_sim[idx]))
 
-        # Get the pairwsie similarity scores of all movies with that movie
-        sim_scores = list(enumerate(cosine_sim[idx]))
+            # Sort the movies based on the similarity scores
+            sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
 
-        # Sort the movies based on the similarity scores
-        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+            # Get the scores of the 10 most similar movies
+            sim_scores = sim_scores[1:11]
 
-        # Get the scores of the 10 most similar movies
-        sim_scores = sim_scores[1:11]
-
-        # Get the movie indices
-        movie_indices = [i[0] for i in sim_scores]
-        print(movie_indices)
-
+            # Get the movie indices
+            movie_indices = [i[0] for i in sim_scores]
+            print(movie_indices)
+        else:
+            movie_indices = []
         # Return the top 10 most similar movies
         # return df2[['title', 'id']].iloc[movie_indices]
         return self.index_movieid.iloc[movie_indices]
