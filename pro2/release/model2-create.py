42985527de90e1e6fa89ae19e947a41b270670ed
Ngo Nhan <ngonhan2k5@gmail.com>
2020-07-08 16:32:11 -0500

    Decrease size of similar matrix Add EDA content base

--- pro2/release/model2-create.py
+++ pro2/release/model2-create.py
@@ -14,13 +14,16 @@ import gzip
 import shutil
 
 # 1. Load data
-# csv_file = '../input/the-movies/movies_metadata.csv'
-csv_file = '../input/tmdb-movie-metadata/tmdb_5000_movies.csv'
-# df2=pd.read_csv(csv_file)
+csv_file = '../input/the-movies/movies_metadata.csv'
+# csv_file = '../input/tmdb-movie-metadata/tmdb_5000_movies.csv'
+df2=pd.read_csv(csv_file)
+df2.drop(df2.columns.difference(['overview','id']), 1, inplace=True)
+# df2.to_csv('../output/the-movies/movies_metadata.csv')
+# df2.to_csv('../output/tmdb-movie-metadata/tmdb_5000_movies.csv')
 
 # df2['overview'].head(1)
 
-
+# quit()
 # 3. Export parts
 from ml_models.model2 import Model2
 
