fa3d02337e3ef6e579bd71cc731696f9b3c9bd75
Ngo Nhan <ngonhan2k5@gmail.com>
2020-07-07 23:05:53 -0500

    Try to use same data set

--- pro2/release/model2-test.py
+++ pro2/release/model2-test.py
@@ -1,12 +1,15 @@
 import pickle
 import pandas as pd 
 
+import sys
 # df2=pd.read_csv('../input/tmdb-movie-metadata/tmdb_5000_movies.csv')
 
 # from app.model2 import Model2
 
 # Test
 id = 155
+if len(sys.argv)>1:
+    id = int(sys.argv[1]) if sys.argv[1].isdigit() else id
 # print(df2.loc[df2['id']==id][['title', 'overview']])
 
 # Import
