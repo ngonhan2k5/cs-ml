d60bdcd933820b97ec4916738929c4f4a60ed3e4
datvt9312 <datvt9312@gmail.com>
2020-07-08 23:20:28 -0500

    save movie data to localhost

--- pro2/frontend/services/movieService.js
+++ pro2/frontend/services/movieService.js
@@ -9,9 +9,16 @@ export async function getMoviesByMovieIds(movieIds) {
   const movies = await axios.get(
     `http://${process.env.host}:5000/api/movies?${movieIdsQuery}`
   );
-
+  const localMovies = []
+  movies.data.forEach(movie => {
+    const localMovie = localStorage.getItem(movie.imdb_id);
+    if(localMovie != null) {
+      localMovies.push(JSON.parse(localMovie))
+    }
+  });
+  movies.data = movies.data.filter((movie) => localStorage.getItem(movie.imdb_id) == null)
   const imbdIds = movies.data.map((movie) => movie.imdb_id);
-  const moviesIds = movies.data.map((movie) => movie.id);
+
   const calls = imbdIds.map((imbdId) =>
     axios.get(`http://www.omdbapi.com/?i=${imbdId}&apikey=${API_KEY}`)
   );
@@ -20,24 +27,34 @@ export async function getMoviesByMovieIds(movieIds) {
   const dataList = results.map((result) => result.data);
     
   for(let i = 0; i < dataList.length; i++){
-    dataList[i]['id'] = movies.data[i]['id'];
-    dataList[i]['overview'] =  movies.data[i]['overview'];
-    dataList[i]['vote_average'] =  movies.data[i]['vote_average'];
-    dataList[i]['vote_count'] =  movies.data[i]['vote_count'];
+    const movie = dataList[i];
+    movie['id'] = movies.data[i]['id'];
+    movie['overview'] =  movies.data[i]['overview'];
+    movie['vote_average'] =  movies.data[i]['vote_average'];
+    movie['vote_count'] =  movies.data[i]['vote_count'];
+    localStorage.setItem(movie.imdbID, JSON.stringify(movie));
   }
 
-  return dataList;
+  return dataList.concat(localMovies);
 }
 
 export async function getMovieByMovieId(movieId) {
 
     const movie = await axios.get(`http://${process.env.host}:5000/api/movies/${movieId}`);
 
-    const result = await axios.get(`http://www.omdbapi.com/?i=${movie.data.imdb_id}&apikey=${API_KEY}`)  
-    result.data.id = movie.data.id
-    result.data.overview = movie.data.overview
-    result.data.vote_average = movie.data.vote_average
-    result.data.vote_count = movie.data.vote_count
-    return result.data;
+    let localMovie = localStorage.getItem(movie.data.imdb_id);
+    if(localMovie == null) {
+      const result = await axios.get(`http://www.omdbapi.com/?i=${movie.data.imdb_id}&apikey=${API_KEY}`)  
+      localMovie = result.data;
+      localMovie.id = movie.data.id
+      localMovie.overview = movie.data.overview
+      localMovie.vote_average = movie.data.vote_average
+      localMovie.vote_count = movie.data.vote_count
+      localStorage.setItem(localMovie.imdbID, JSON.stringify(localMovie));
+    } else {
+      localMovie = JSON.parse(localMovie)
+    }
+
+    return localMovie;
   }
   
