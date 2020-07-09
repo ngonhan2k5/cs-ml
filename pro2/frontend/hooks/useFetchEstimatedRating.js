af0abf7426d55b24b6a79781a53d4cd3eb72e607
datvt9312 <datvt9312@gmail.com>
2020-07-08 19:01:14 -0500

    update localhost to env host

--- pro2/frontend/hooks/useFetchEstimatedRating.js
+++ pro2/frontend/hooks/useFetchEstimatedRating.js
@@ -17,7 +17,7 @@ function useFetchEstimatedRating({ movieId, userId }) {
       setData(null);
       try {
         const result = await axios.get(
-          `http://127.0.0.1:5000/api/estimated_rate/${userId}/${movieId}`
+          `http://${process.env.host}/api/estimated_rate/${userId}/${movieId}`
         );
 
         setData(result.data);
