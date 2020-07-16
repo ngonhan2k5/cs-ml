962531bd62574c1937a4dc786d2000b5378e60a0
datvt9312 <datvt9312@gmail.com>
2020-07-15 22:43:00 -0500

    update UI

--- proj3/frontend/components/ImagePredictForm.js
+++ proj3/frontend/components/ImagePredictForm.js
@@ -1,6 +1,8 @@
 import { useRef, useState } from "react";
 import Head from "next/head";
 import axios from "axios";
+import NProgress from "nprogress"; //nprogress module
+
 import Checkbox from "./Checkbox";
 
 export default function ImagePredictForm(props) {
@@ -34,36 +36,46 @@ export default function ImagePredictForm(props) {
     });
     // console.log(response);
     // console.log("response" + response);
-    const data = response.data.prediction;
+    const data = response.data;
 
     return data;
   };
 
   const handlePredict = async () => {
-    if(predicts.length === 0){
-      return alert("Must choose at least one prediction model")
+    NProgress.start();
+    if (predicts.length === 0) {
+      return alert("Must choose at least one prediction model");
     }
-    if(imageSrc.length === 0){
-      return alert("Must select picture to predict")
+    if (imageSrc.length === 0) {
+      return alert("Must select picture to predict");
     }
     try {
       props.setData([]);
       const fetchDataList = predicts.map((predict) => fetchData(predict));
       const dataList = await Promise.all(fetchDataList);
       const newDataList = [];
+      const newExectionTimeList = [];
       for (let i = 0; i < dataList.length; i++) {
         newDataList.push({
           name: predicts[i],
-          dog: Math.round(dataList[i]["dog"] * 10000) / 10000,
-          cat: Math.round(dataList[i]["cat"] * 10000) / 10000,
+          dog: Math.round(dataList[i]["prediction"]["dog"] * 10000) / 10000,
+          cat: Math.round(dataList[i]["prediction"]["cat"] * 10000) / 10000,
+        });
+        newExectionTimeList.push({
+          name: predicts[i],
+          execution_time:
+            Math.round(dataList[i]["execution_time"] * 10000) / 10000,
         });
       }
-      console.log(dataList);
       console.log(newDataList);
+      console.log(newExectionTimeList);
       props.setData(newDataList);
+      props.setExectionTimes(newExectionTimeList);
     } catch (e) {
       console.log("e" + e);
+      alert(e);
     }
+    NProgress.done();
   };
 
   const selectedCheckboxes = new Set();
@@ -83,23 +95,73 @@ export default function ImagePredictForm(props) {
     "predict_mobilenet",
   ];
   return (
-    <div className="col">
-      <img src={imageSrc} />
-      <div>
-        <input type="file" onChange={handleFileChanged} />
-        {selections.map((predict) => {
-          console.log("predicts.indexOf(predict)" + predicts.indexOf(predict));
-          return (
-            <Checkbox
-              label={predict}
-              isSelected={predicts.indexOf(predict) > -1}
-              handleCheckboxChange={toggleCheckbox}
-              key={predict}
-            />
-          );
-        })}
-        <button onClick={handlePredict}>Predict</button>
+    <div>
+      <div className="row input-area">
+        <div className="col-12 col-md-6">
+          <h4 className="predicts-selection-label">Step 1: Select image to predict</h4>
+          {imageSrc.length > 0 ? (
+            <img src={imageSrc} />
+          ) : (
+            <div className="img-placeholder">
+              <span>No image is selected</span>
+            </div>
+          )}
+          <input type="file" onChange={handleFileChanged} />
+        </div>
+        <div className="col-12 col-md-6">
+          <h4 className="predicts-selection-label">
+            Step 2: Select model(s) to predict
+          </h4>
+          {selections.map((predict) => {
+            console.log(
+              "predicts.indexOf(predict)" + predicts.indexOf(predict)
+            );
+            return (
+              <Checkbox
+                label={predict}
+                isSelected={predicts.indexOf(predict) > -1}
+                handleCheckboxChange={toggleCheckbox}
+                key={predict}
+              />
+            );
+          })}
+        </div>
       </div>
+      <button onClick={handlePredict}>Predict</button>
+
+      <style jsx>{`
+        .input-area {
+          border: 1px solid #ccc;
+          padding: 10px 10px 30px 10px;
+          border-radius: 5px;
+        }
+        .img-placeholder {
+          widht: 300px;
+          height: 300px;
+          background-color: #ccc;
+          position: relative;
+        }
+        .img-placeholder span {
+          display: block;
+          position: absolute;
+          transition: lef;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+        }
+        .predicts-selection-label {
+          display: block;
+          margin-top: 17px;
+          margin-bottom: 8px;
+          font-weight: 500;
+        }
+        button,
+        input {
+          margin-top: 25px;
+          border: 1px solid #ccc;
+          padding: 5px 10px;
+        }
+      `}</style>
     </div>
   );
 }
--- proj3/frontend/pages/_app.js
+++ proj3/frontend/pages/_app.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Head from "next/head";
+import 'nprogress/nprogress.css';
 
 export default function MyApp({ Component, pageProps }) {
   return (
